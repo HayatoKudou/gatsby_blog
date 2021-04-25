3506938d1b9de7d9c7ccefc4c1787e12ebcfc04f
kudou <kudoh115@gmail.com>
2021-04-25 20:34:37 +0900

    master

--- src/components/PageLayout/Sidebar/Three.js/three.jsx
+++ src/components/PageLayout/Sidebar/Three.js/three.jsx
@@ -80,7 +80,7 @@ function Box(props) {
 
 function isSmartPhone() {
     // UserAgentからのスマホ判定
-    if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+    if (navigator != 'undefind' && navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
         return true;
     } else {
         return false;
